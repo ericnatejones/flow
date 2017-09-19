db8aa2c60d7bd0e639efcec3a7a01f25654518a1
Eric Jonese <ericnatejones@gmail.com>
2017-09-19 10:34:12 -0600

    I'm starting this back up after 6 months. Seems to be able to add users, add streams, edit favorites. Backend only for now

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -5,30 +5,46 @@ var Stream = require("../models/stream")
 var jwt = require("jsonwebtoken");
 var config = require("../config");
 
+favoriteRouter.route("/param/:which/:streamId")
+    .put(function(req, res){
+        console.log(req.user)
+        User.find({_id: req.user._id}, function (err, user) {
+          if (err) console.log("err" + err);
+          console.log("favoriteStreams" + user);
+          console.log("++++++++")
+            user.favoriteStreams.forEach(function(stream){
+                console.log("Success, stream" + stream);
+                if (stream._id === req.param.streamId){
+                    stream[req.param.which] === req.body.param;
+                    user.save(function (err, savedUser) {
+                        if (err) res.status(500).send(err);
+                        res.status(201).send(savedUser);
+                    });
+                }
+            })
+            res.send("Stream is not favorited by user")
+        });
+    })
+
 favoriteRouter.route("/:streamId")
     .put(function(req, res){
-        var stream = Stream.findById(req.params.streamId);
+        Stream.findById(req.params.streamId, function(err, stream){
+          if (err) console.log(`error:${err}`);
 
-        req.user.favoriteStreams.push(stream);
-        console.log(req.user);
+          console.log(req.user.favoriteStreams);
 
-        User.findByIdAndUpdate(req.user._id, req.user, {new: true}, function (err, user) {
-            if (err) res.status(500).send(err);
-            res.send(user);
+          req.user.favoriteStreams.push({stream: stream});
+
+          console.log(req.user.favoriteStreams)
+          User.findByIdAndUpdate(req.user._id, req.user, {new: true}, function (err, user) {
+            console.log("success")
+              if (err) res.status(500).send(err);
+              res.send(user);
+          });
         });
+
     })
 
-favoriteRouter.route("param/:which/:streamId")
-    .put(function(req, res){
-        var stream = Stream.findById(req.params.streamId);
 
-        req.user.favoriteStreams.push(stream);
-        console.log(req.user);
-
-        User.findByIdAndUpdate(req.user._id, req.user, {new: true}, function (err, user) {
-            if (err) res.status(500).send(err);
-            res.send(user);
-        });
-    })
 
 module.exports = favoriteRouter;
