05299fabde7cb90174a7ec2a06857ebc8e2dc03c
Eric Jonese <ericnatejones@gmail.com>
2017-12-07 15:01:04 -0700

    restored server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+ const express = require("express");
+ const cors = require("cors");
+ const app = express();
+ const path = require("path");
+ const morgan = require("morgan");
+ const mongoose = require("mongoose");
+ const bodyParser = require("body-parser");
+ const config = require("./config");
+ const expressJwt = require("express-jwt");		
+
+ const port = process.env.PORT || 5000;
+
+ app.use(cors());
+
+ app.use(morgan("dev"));
+ app.use(bodyParser.json());
+
+
+ app.use(express.static(path.join(__dirname, "public")));
+
+ mongoose.connect(config.database, err => {
+     if (err) throw err;
+     console.log("Successfully connected to the database");
+ });
+
+ app.use("/api", expressJwt({secret: process.env.SECRET || config.secret}));
+
+ app.use("/api/favorite", require("./routes/favoriteRoutes"));
+
+ app.use("/stream", require("./routes/streamRoutes"));
+
+ app.use("/auth", require("./routes/authRoutes"));
+
+ app.listen(port, () => {
+     console.log(`Server listening on port ${port}`);
+ });
